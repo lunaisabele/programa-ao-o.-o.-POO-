3209ccb7f9404c462f0518bf66c2ef92712e2c6c
luna <isabeleluna16@gmail.com>
2025-05-14 16:25:26 -0300

    continuaçao

--- biblioteca.py
+++ biblioteca.py
@@ -45,5 +45,108 @@ class ClassePessoa():
         else:
             self.falando = True
             print(f"{self.nome} está falando")
+#________________________________________________________________________________
+class Animal():
+    def __init__(self,nome,cor):
+        self.nome = nome
+        self.cor = cor
+
+    def comer(self):
+        print (f"O {self.nome} foi comer...")
+
+class Gato(Animal):
+    def __init__(self,nome,cor):
+        super().__init__(nome,cor)
+
+    def miar(self):
+        print(f"O {self.nome} foi miar...")
+
+class Cachorro(Animal):
+    def __init__(self,nome,cor):
+        self.nome = nome
+        self.cor = cor
+
+    def comer(self):
+        print(f"{self.nome} foi comer...")
+
+    def latir(self):
+        print(f"{self.nome} foi latir...")
+
+class Coelho(Animal):
+    def __init__(self,nome,cor):
+        self.nome = nome
+        self.cor = cor
+
+    def comer(self):
+        print(f"{self.nome} foi comer...")
+
+    def grunir(self):
+        print(f"{self.nome} foi grunir...")
+
+
+class Vaca(Animal):
+    def __init__(self, nome, cor):
+        self.nome = nome
+        self.cor = cor
+
+    def comer(self):
+        print(f"{self.nome} foi comer...")
+
+    def mugir(self):
+        print(f"{self.nome} foi mugir...")
+
+class Ingresso:
+    def __init__(self,valor):
+        self.valor = valor
+
+    def imprimirValor(self):
+        print(f"O valor do ingresso é: R${self.valor}")
+
+
+class IngressoVip(Ingresso):
+    def __init__(self,valor):
+        super().__init__(valor*1.5)
+
+
+class Forma():
+    def __init__(self):
+        self.area = 0
+        self.perimetro = 0
+
+class Retangulo(Forma):
+    def __init__(self,base,altura):
+        self.base = base
+        self.altura = altura
+
+
+    def calculoArea(self):
+        self.area = (self.base * self.altura)
+        print(f"A area do retango é: {self.area}")
+
+
+    def calculoPerimetro(self):
+        self.perimetro = (2*self.base + self.altura)
+        print(f"O perimetro do retangulo é: {self.altura}")
+
+
+class Triangulo(Forma):
+    def __init__(self,altura,base):
+       self.altura = altura
+       self.base = base
+
+    def calculaArea(self):
+        self.area = (self.base * self.altura/2)
+
+    def calculaPerimetro(self):
+        self.altura = (self.base*3)
+
+
+
+
+
+
+
+
+
 
 
--- main.py
+++ main.py
@@ -6,3 +6,29 @@ print(aluno01.nome)
 aluno01.Dormir()
 aluno01.Dormir()
 aluno01.Falar()
+
+gato = Gato("Jhonny","cinza")
+gato.miar()
+gato.comer()
+
+cachorro = Cachorro("Poty","preto")
+cachorro.comer()
+cachorro.latir()
+
+coelho = Coelho("Mococa","preto")
+coelho.comer()
+coelho.grunir()
+
+vaca = Vaca("Mimosa","malhada")
+vaca.comer()
+vaca.mugir()
+
+ingresso = Ingresso(20)
+ingresso.imprimirValor()
+
+ingressovip = IngressoVip(20)
+ingressovip.imprimirValor()
+
+triangulo = Retangulo(20,40)
+triangulo.calculoArea()
+
